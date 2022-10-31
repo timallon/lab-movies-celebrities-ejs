c5a05392d6c77a4275c2cdbf72ff5798874a0ede
Tim Allon <timallon@gmail.com>
2022-10-31 16:52:44 +0000

    iteration-0 done

--- db/index.js
+++ db/index.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-movies-celebrities";
+//const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-movies-celebrities";
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://tim:tim@cluster0.0qcxtpa.mongodb.net/lab-movies-celebrities";
+
 
 mongoose
   .connect(MONGO_URI)
